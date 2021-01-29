bb621b69fcd8a1f644af29486ea847a0c8e8e998
Rich South <richard.south@gmail.com>
2021-01-29 12:59:44 +0000

    Updating qtile

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -2,6 +2,7 @@
 picom &
 variety &
 syncthing-gtk -m &
+#flameshot
 ~/.config/polybar/launch.sh &
 # mopidy --config ~/.config/mopidy/mopidy.conf
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
